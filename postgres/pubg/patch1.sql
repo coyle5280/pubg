59a6b29ddbe5a7027646ff61a541bd2815b32f98
josh_coyle-at-949264846382 <coyle5280@gmail.com>
2018-04-28 16:45:16 -0600

    added telemetry tables to db

--- postgres/pubg/patch1.sql
+++ postgres/pubg/patch1.sql
@@ -62,3 +62,75 @@ ALTER TABLE pubg.match
 
     Alter table pubg.match_record
 ALTER COLUMN ride_distance TYPE double precision;
+
+
+CREATE TABLE pubg.match_attack_damage
+(
+    attacker_name character varying(50) NOT NULL,
+    attacker_account_id character varying(90),
+    attacker_team_id bigint,
+    attacker_health double precision,
+    attacker_x double precision,
+    attacker_y double precision,
+    attacker_z double precision,
+    attack_type character varying(50),
+    attack_weapon jsonb,
+    attack_vehicle jsonb,
+    attack_time timestamp(50) without time zone,
+    attack_version bigint,
+    victim_name character varying,
+    victim_id character varying(90),
+    victim_team_id bigint,
+    victim_health double precision,
+    vimtim_x double precision,
+    victim_y double precision,
+    victim_z double precision,
+    damage_type_category character varying(50),
+    damage_reason character varying(50),
+    damage double precision,
+    damage_causer_name character varying(50),
+    damage_time timestamp(50) without time zone,
+    damage_version bigint,
+    attack_id double precision NOT NULL
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE pubg.match_attack_damage
+    OWNER to postgres;
+
+
+CREATE TABLE pubg.match_player_position
+(
+    player_id character varying(90) NOT NULL,
+    player_x double precision,
+    player_y double precision,
+    player_z double precision,
+    player_rank bigint,
+    elapsed_time bigint,
+    number_players_alive bigint,
+    position_version bigint,
+    position_time_stamp timestamp(10) without time zone,
+    match_id character varying(90) NOT NULL
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE pubg.match_player_position
+    OWNER to postgres;
+
+ALTER TABLE pubg.match_attack_damage
+    ADD COLUMN attack_uuid uuid NOT NULL;
+
+ALTER TABLE pubg.match_attack_damage
+    ADD PRIMARY KEY (attack_uuid);
+
+ALTER TABLE pubg.match_player_position
+    ADD COLUMN match_position_uuid uuid NOT NULL;
+
+ALTER TABLE pubg.match_player_position
+    ADD PRIMARY KEY (match_position_uuid);
