a70fcddc0074c87b288579d9673965d21ecc6c47
josh_coyle-at-949264846382 <coyle5280@gmail.com>
2018-05-13 23:48:41 -0600

    records

--- PubgTeamPage/PubgTeamPage/app/view/main/records/Records.js
+++ PubgTeamPage/PubgTeamPage/app/view/main/records/Records.js
@@ -4,7 +4,7 @@ Ext.define('PubgTeamPage.view.main.records.Records', {
     },
     controller: 'records',
     extend: 'Ext.view.View',
-    itemSelector: 'div',
+    itemSelector: 'div.main',
     listeners: {
         select: 'recordSelect'
     },
@@ -15,37 +15,82 @@ Ext.define('PubgTeamPage.view.main.records.Records', {
     ],
     tpl: new Ext.XTemplate(`
         <tpl for=".">
-            <div class="records">'
-                <h4 class="record-header">Most Kills Duo:</h4>
-                    <ul>
-                        <tpl for="kills">
-                            <li>{most_kill_name} @ {kills}</li>
-                        </tpl>
-                    </ul>
-                <h4 class="record-header">Most Kills Any Mode:</h4>
-                    <ul>
-                        <tpl for="kills_all">
-                            <li>{most_kill_name} @ {kills}</li>
-                        </tpl>
-                    </ul>
-                <h4 class="record-header">Most Assists Duo:</h4>
-                    <ul>
-                        <tpl for="query_assists_duo">
-                            <li>{name} @ {assists}</li>
-                        </tpl>
-                    </ul>
-                <h4 class="record-header">Total Kills Leader:</h4>
-                    <ul>
-                        <tpl for="total_kills">
-                            <li>{name} @ {total}</li>
-                        </tpl>
-                    </ul>
-                <h4 class="record-header">Total Head Shots Leader:</h4>
-                    <ul>
-                        <tpl for="head_shot_kills">
-                            <li>{name} @ {total}</li>
-                        </tpl>
-                    </ul>
+            <div class="main">'
+                <div class="column">'
+                    <h3 class="record-header">Solo Stats</h3>
+                    <h4 class="record-header">Most Kills:</h4>
+                        <ul>
+                            <tpl for="most_kills_solo">
+                                <li>{most_kill_name} @ {kills}</li>
+                            </tpl>
+                        </ul>
+                    <h4 class="record-header">Most Assists:</h4>
+                        <ul>
+                            <tpl for="most_assists_solo">
+                                <li>{most_assists_name} @ {assists}</li>
+                            </tpl>
+                        </ul>
+                    <h4 class="record-header">Longest Kill:</h4>
+                        <ul>
+                            <tpl for="longest_kill_solo">
+                                <li>{longest_kill_name} @ {longest_kill}</li>
+                            </tpl>
+                        </ul>
+                </div>
+                <div class="column">'
+                    <h3 class="record-header">Duo Stats</h3>
+                    <h4 class="record-header">Kills in a single game: </h4>
+                        <ul>
+                            <tpl for="most_kills_duo">
+                                <li>{most_kill_name} @ {kills}</li>
+                            </tpl>
+                        </ul>
+                    <h4 class="record-header">Longest Kill:</h4>
+                        <ul>
+                            <tpl for="longest_kill_duo">
+                                <li>{longest_kill_name} @ {longest_kill}</li>
+                            </tpl>
+                        </ul>
+                    <h4 class="record-header">Most Assists Duo:</h4>
+                        <ul>
+                            <tpl for="most_assists_duo">
+                                <li>{most_assists_name} @ {assists}</li>
+                            </tpl>
+                        </ul>
+                    <h4 class="record-header">Most Revives:</h4>
+                        <ul>
+                            <tpl for="most_revives_duo">
+                                <li>{most_revives_name} @ {revives}</li>
+                            </tpl>
+                        </ul>
+                </div>
+                <div class="column">'
+                    <h3 class="record-header">Squad Stats</h3>
+                    <h4 class="record-header">Kills in a single game: </h4>
+                        <ul>
+                            <tpl for="most_kills_squad">
+                                <li>{most_kill_name} @ {kills}</li>
+                            </tpl>
+                        </ul>
+                    <h4 class="record-header">Longest Kill:</h4>
+                        <ul>
+                            <tpl for="longest_kill_squad">
+                                <li>{longest_kill_name} @ {longest_kill}</li>
+                            </tpl>
+                        </ul>
+                    <h4 class="record-header">Most Assists:</h4>
+                        <ul>
+                            <tpl for="most_assists_squad">
+                                <li>{most_assists_name} @ {assists}</li>
+                            </tpl>
+                        </ul>
+                    <h4 class="record-header">Most Revives:</h4>
+                        <ul>
+                            <tpl for="most_revives_squad">
+                                <li>{most_revives_name} @ {revives}</li>
+                            </tpl>
+                        </ul>
+                </div>
             </div>
         </tpl>
     `),
