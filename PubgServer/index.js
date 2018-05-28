39d0ac7682ff43586768ef5b30e86d41e92d727f
josh_coyle-at-949264846382 <coyle5280@gmail.com>
2018-05-28 13:33:20 -0600

    Added routes for maps and added helper function for multiple queries

--- PubgServer/index.js
+++ PubgServer/index.js
@@ -46,6 +46,20 @@ app.get('/matches', async function (req, res) {
     }
 })
 
+app.get('/matches/modes', async (req, res) => {
+    const query = require('./sql/matches/match_modes.sql')
+    try {
+        let response = await helperFunctions.queryExecute({
+            params: [],
+            pool, 
+            query
+        })
+        res.json(response)
+    } catch (err) {
+        res.status(500).send({ err })
+    }
+})
+
 app.get('/matches/:match_id', async (req, res) => {
     const query = require('./sql/matches/match_details_single.sql')
     try {
@@ -102,74 +116,116 @@ app.get('/records', async (req, res) => {
     var most_kills = require('./sql/records/most_kills.sql')
     var most_revives = require('./sql/records/most_revives.sql')
     var most_assists = require('./sql/records/most_assists.sql')
-    let result = {}
+    let queries = {}
+    let params = {}
+    
+    queries = {
+        longest_kill_duo: longest_kill,
+        longest_kill_squad: longest_kill,
+        longest_kill_solo: longest_kill,
+        most_kills_duo: most_kills,
+        most_kills_squad: most_kills,
+        most_kills_solo: most_kills,
+        most_revives_duo: most_revives,
+        most_revives_squad: most_revives,
+        most_revives_solo: most_revives,
+        most_assists_duo: most_assists,
+        most_assists_squad: most_assists,
+        most_assists_solo: most_assists
+    }
+
+    params = {
+        longest_kill_duo: ['duo-fpp'],
+        longest_kill_squad: ['squad-fpp'],
+        longest_kill_solo: ['solo-fpp'],
+        most_kills_duo: ['duo-fpp'],
+        most_kills_squad: ['squad-fpp'],
+        most_kills_solo: ['solo-fpp'],
+        most_revives_duo: ['duo-fpp'],
+        most_revives_squad: ['squad-fpp'],
+        most_revives_solo: ['solo-fpp'],
+        most_assists_duo: ['duo-fpp'],
+        most_assists_squad: ['squad-fpp'],
+        most_assists_solo: ['solo-fpp']
+    }
+    
     try {
-        result = {
-            longest_kill_duo: await helperFunctions.queryExecute({
-                params: ['duo-fpp'],
-                pool, 
-                query: longest_kill
-            }),
-            longest_kill_squad: await helperFunctions.queryExecute({
-                params: ['squad-fpp'],
-                pool, 
-                query: longest_kill
-            }),
-            longest_kill_solo: await helperFunctions.queryExecute({
-                params: ['solo-fpp'],
-                pool, 
-                query: longest_kill
-            }),
-            most_kills_duo: await helperFunctions.queryExecute({
-                params: ['duo-fpp'],
-                pool, 
-                query: most_kills
-            }),
-            most_kills_squad: await helperFunctions.queryExecute({
-                params: ['squad-fpp'],
-                pool, 
-                query: most_kills
-            }),
-            most_kills_solo: await helperFunctions.queryExecute({
-                params: ['solo-fpp'],
-                pool, 
-                query: most_kills
-            }),
-            most_revives_duo: await helperFunctions.queryExecute({
-                params: ['duo-fpp'],
-                pool, 
-                query: most_revives
-            }),
-            most_revives_squad: await helperFunctions.queryExecute({
-                params: ['squad-fpp'],
-                pool, 
-                query: most_revives
-            }),
-            most_revives_solo: await helperFunctions.queryExecute({
-                params: ['solo-fpp'],
-                pool, 
-                query: most_revives
-            }),
-            most_assists_duo: await helperFunctions.queryExecute({
-                params: ['duo-fpp'],
-                pool, 
-                query: most_assists
-            }),
-            most_assists_squad: await helperFunctions.queryExecute({
-                params: ['squad-fpp'],
-                pool, 
-                query: most_assists
-            }),
-            most_assists_solo: await helperFunctions.queryExecute({
-                params: ['solo-fpp'],
-                pool, 
-                query: most_assists
-            })
-        }
-        res.json(result)
+        let response = await helperFunctions.multiQueryExecute({
+            params,
+            pool, 
+            queries
+        })
+        res.json(response)
     } catch (err) {
         res.status(500).send({ err })
     }
+    // try {
+    //     result = {
+    //         longest_kill_duo: await helperFunctions.queryExecute({
+    //             params: ['duo-fpp'],
+    //             pool, 
+    //             query: longest_kill
+    //         }),
+    //         longest_kill_squad: await helperFunctions.queryExecute({
+    //             params: ['squad-fpp'],
+    //             pool, 
+    //             query: longest_kill
+    //         }),
+    //         longest_kill_solo: await helperFunctions.queryExecute({
+    //             params: ['solo-fpp'],
+    //             pool, 
+    //             query: longest_kill
+    //         }),
+    //         most_kills_duo: await helperFunctions.queryExecute({
+    //             params: ['duo-fpp'],
+    //             pool, 
+    //             query: most_kills
+    //         }),
+    //         most_kills_squad: await helperFunctions.queryExecute({
+    //             params: ['squad-fpp'],
+    //             pool, 
+    //             query: most_kills
+    //         }),
+    //         most_kills_solo: await helperFunctions.queryExecute({
+    //             params: ['solo-fpp'],
+    //             pool, 
+    //             query: most_kills
+    //         }),
+    //         most_revives_duo: await helperFunctions.queryExecute({
+    //             params: ['duo-fpp'],
+    //             pool, 
+    //             query: most_revives
+    //         }),
+    //         most_revives_squad: await helperFunctions.queryExecute({
+    //             params: ['squad-fpp'],
+    //             pool, 
+    //             query: most_revives
+    //         }),
+    //         most_revives_solo: await helperFunctions.queryExecute({
+    //             params: ['solo-fpp'],
+    //             pool, 
+    //             query: most_revives
+    //         }),
+    //         most_assists_duo: await helperFunctions.queryExecute({
+    //             params: ['duo-fpp'],
+    //             pool, 
+    //             query: most_assists
+    //         }),
+    //         most_assists_squad: await helperFunctions.queryExecute({
+    //             params: ['squad-fpp'],
+    //             pool, 
+    //             query: most_assists
+    //         }),
+    //         most_assists_solo: await helperFunctions.queryExecute({
+    //             params: ['solo-fpp'],
+    //             pool, 
+    //             query: most_assists
+    //         })
+    //     }
+    //     res.json(result)
+    // } catch (err) {
+    //     res.status(500).send({ err })
+    // }
 })
 
 app.get('/players', async (req, res) => {
@@ -261,6 +317,24 @@ app.get('/tiles/256/:map/:z/:x/:y', (req, res) => {
         res.status(500).json(error)
     }
 })
+
+app.get('/map/:mapname/:player_id', async (req, res) => {
+    try {
+        const query = require(helperFunctions.getMapLayerQuery(req.query.type))
+        const params = [req.params.player_id,req.query.mode,req.params.mapname]
+        let response = await helperFunctions.queryExecute({
+            params,
+            pool, 
+            query
+        })
+        let data = (req.query.layertype === 'point') ?  helperFunctions.geojson(response) : helperFunctions.heat(response)
+        res.json(data)
+    } catch (err) {
+        res.status(500).send({ err })
+    }
+})
+
+
 
 app.listen('80', function () {
     logger.log('PubgAnalytics API listening on port:', '80')
